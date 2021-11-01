037f1161e0c2602f9813336218077fdd50ac54cf
unknown <dilu.kefale@gmail.com>
2021-11-01 12:31:19 +0300

    github api with basic strings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
-
+import Search from './components/search';
+import styled from 'styled-components';
+import Select from 'react-select';
+import { useState } from 'react';
 function App() {
+  const Div=styled.div`
+ background-color: #282c34;
+  padding:10% 0;
+  `
+  const Input=styled.input`
+ padding:1% 30%;
+border-radius:15px;
+  
+  `
+  const languageList=[{
+value:'javascript',label:'javascript'
+
+  },
+  {
+    value:'tetris',label:'tetris'
+    
+      },{
+        value:'java',label:'java'
+        
+          }]
+          const [selected,setSelected]=useState('tetris')
+          const listHandler=event=>{
+    setSelected(event)
+          }
   return (
-    <div className="App">
+    <Div >
       <header className="App-header">
-       <input placeholder='searh'/>
+        <label ><strong> SEARCH GITHUB REPO </strong></label>
+       <Input placeholder='search'/>
+       <Select options={languageList} onChange={value=>listHandler(value)} value={selected}/>
+       <Search qlanguage={selected} qstring='tetris'/>
+       
       </header>
-    </div>
+    </Div>
   );
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 
-const search = () => {
+const Search = ({qstring,qlanguage}) => {
+    const [result,setResult]=useState([])
+    let url='https://api.github.com/search/repositories?q='
+   let qurl=url+qstring +'+'+'language:'+qlanguage+'&sort=stars&order=des'
+    useEffect(() => {
+fetch(qurl).then(res=>res.json()).then(data=>{setResult(data.items) 
+    console.log(data.items   )})       
+    }, [qurl])
     return (
         <div>
-            
+            {result.map(s=>{
+                return <><h4>{s.full_name}</h4>
+                <span><p>{s.description}</p></span>
+                </>
+            })}
         </div>
     )
 }
 
-export default search
+export default Search
+
