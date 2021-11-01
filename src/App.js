80ad42884a2e0e2677d344dea26eca4358137183
unknown <dilu.kefale@gmail.com>
2021-11-01 14:21:19 +0300

    automatic search handler

--- src/App.js
+++ src/App.js
@@ -24,17 +24,22 @@ value:'javascript',label:'javascript'
         value:'java',label:'java'
         
           }]
-          const [selected,setSelected]=useState('tetris')
+          const [selected,setSelected]=useState('javascript')
+          const [searchString,setSearchString]=useState('react')
+          const searchHandler=event=>{
+            setSearchString(event.target.value)
+          }
           const listHandler=event=>{
+            event.preventDefault()
     setSelected(event)
           }
   return (
     <Div >
       <header className="App-header">
         <label ><strong> SEARCH GITHUB REPO </strong></label>
-       <Input placeholder='search'/>
+       <Input placeholder='search' onChange={searchHandler} value={searchString}/>
        <Select options={languageList} onChange={value=>listHandler(value)} value={selected}/>
-       <Search qlanguage={selected} qstring='tetris'/>
+       <Search qlanguage={selected} qstring={searchString}/>
        
       </header>
     </Div>
